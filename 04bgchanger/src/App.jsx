9af3125aaa0777bd10d5aa40269448f21cd2fdf7
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2025-07-28 07:39:47 +0530

    useState,useRef,useCallBack

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,51 @@
+
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [color, setColour] = useState("olive")
+
+  return (
+    <>
+      <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+            <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+              <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+                <button
+                onClick={() => setColour("red")}
+                 className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"red"
+                }}>Red</button>
+                <button
+                onClick={() => setColour("green")}
+                 className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"green"
+                }}>Red</button>
+                <button
+                onClick={() => setColour("pink")} 
+                className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"pink"
+                }}>Red</button>
+                <button
+                onClick={() => setColour("blue")}
+                 className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"blue"
+                }}>Red</button>
+                <button
+                onClick={() => setColour("yellow")}
+                 className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"yellow"
+                }}>Red</button>
+                <button
+                onClick={() => setColour("orange")}
+                 className='outline-none px-4 py-1 rounded-full text-white shadow-sm' style={{
+                  backgroundColor:"orange"
+                }}>Red</button>
+              </div>
+            </div>
+      </div>
+    </>
+  )
+}
+
+export default App
