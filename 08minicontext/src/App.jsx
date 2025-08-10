96d399e8dd041b5c8d34ab5a0c880c3ba6b258d8
sushil-kumarsahoo <suhilkmrsho@gmail.com>
2025-08-10 10:33:16 +0530

    context api discussion

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextprovider'
+
+function App() {
+  
+  return (
+    <UserContextProvider>
+     <h1>context api </h1>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
